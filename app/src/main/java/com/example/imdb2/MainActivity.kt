1fe1e1e99c96dde7f2513ebcb224db879c97384c
Santiago Mosquera Mapura <s.mosquera@globant.com>
2023-09-23 14:30:28 -0500

    Delete activity to splash, because I prefered to use API splash screen from Android

--- app/src/main/java/com/example/imdb2/MainActivity.kt
+++ app/src/main/java/com/example/imdb2/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.imdb2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }

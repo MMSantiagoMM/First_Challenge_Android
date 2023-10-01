e11adba55d2e67aa9b6bcea669a3a9bbf54c90c2
Santiago Mosquera Mapura <s.mosquera@globant.com>
2023-10-01 14:10:52 -0500

    Doing some changes in splash screen

--- app/src/main/java/com/example/imdb2/MainActivity.kt
+++ app/src/main/java/com/example/imdb2/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.imdb2
 
 
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -15,15 +21,12 @@ import com.example.imdb2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(1000)
         installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
     }
 
 
